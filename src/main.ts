dad02a82f2fd18cb9e031b2a9709914183346894
SSSJCLASS <133522108+SSSJCLASS@users.noreply.github.com>
2023-07-24 21:12:15 +0300

    / let login =prompt()

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,75 @@ import './style.css'
 // var b=80
 // var c=a+b
 // alert("Уровнение: " + c)
+
+
+
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '')
+
+// if (year == '2015') {
+//     alert( 'Да вы знаток!' )
+// } else {
+//     alert( 'А вот и неправильно!' )
+// }
+// let name = prompt('„Какое «официальное» название JavaScript?“','')
+
+// if (name == 'ECMAScript' ) {
+//     alert("Верно :) !")
+// } else {
+//     alert('Не знаете? “ECMAScript”!')
+// }
+
+
+
+
+// let value = (prompt('Ввидите число',) as string )
+
+// if (value > '0'){
+//     alert('1')
+// } else if (value < '0')
+//     alert(-1)
+// else{
+//     value == '0'
+//     alert('0')
+// }
+
+
+
+
+
+
+// let result = ''
+//     a = ''
+//     b = ''
+
+// result = (a + b < '4') ? 'Мало' : 'Много' ;
+
+
+
+
+
+
+
+
+
+// let login =prompt() 
+
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//     (login == 'Директор') ? 'Здравствуйте' :
+//     (login == '') ? 'Нет логина' :
+//     '';
+// alert(message)
+
+
+
+
+// let age = prompt('Введите ваш возраст') 
+// function whoAreYou(age:any) {
+//     if(age>=0&&age<3) console.log('ты ребенок') 
+//     else if(age>=3&&age<12) console.log('ты yung')
+//     else if(age>=12&&age<19) console.log('ты подросток')
+//     else if(age>18&&age<61) console.log('вы взрослый')
+//     else if(age>60) console.log('вы очень постарели')
+// }
+// whoAreYou(age)
